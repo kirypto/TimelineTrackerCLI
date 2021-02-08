d2bc2007e6fd71c59b379a9077ca8e5bed436851
kirypto <kirypto@gmail.com>
2021-02-07 18:44:14 -0600

    Get retrieval from the timeline tracker api working

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
+from json import dumps
+
+from timeline_tracker_gateway import TimelineTrackerGateway
+
+
 def _main():
+    url = "http://172.16.1.101:1337"
+    gateway = TimelineTrackerGateway(url)
+
     while True:
-        print()
-    pass
+        print("------------------------------")
+        print("Available:")
+        print("1. Get Location")
+        print("2. Create Location")
+        command = int(input("Input command: "))
+        if command == 1:
+            location_id = input("Input location id: ")
+            print(dumps(gateway.get_location(location_id), indent=2))
+        elif command == 2:
+            raise NotImplementedError("Welp... -_-")
+        else:
+            print(f"ERROR: Unknown command '{command}'")
 
 
 if __name__ == '__main__':
--- timeline_tracker_gateway.py
+++ timeline_tracker_gateway.py
@@ -1,1 +1,25 @@
+from http import HTTPStatus
+from typing import Dict, Any
+
 import requests
+
+
+class TimelineTrackerGateway:
+    _url: str
+
+    def __init__(self, url: str):
+        self._url = url
+
+    def post_location(self, location_id: str, location_json: Dict[str, Any]) -> None:
+        url = f"{self._url}/api/location/{location_id}"
+        response = requests.post(url, json=location_json)
+        if response.status_code != HTTPStatus.CREATED:
+            raise RuntimeError(f"Failed to post location: {response.text}")
+
+    def get_location(self, location_id: str) -> Dict[str, Any]:
+        url = f"{self._url}/api/location/{location_id}"
+        response = requests.get(url)
+        if response.status_code != HTTPStatus.OK:
+            raise RuntimeError(f"Failed to post location: {response.text}")
+        return response.json()
+
